d97d073c9518c6f0f3ada5165a534300d3fb0735
amina-safdar <58341986+amina-safdar@users.noreply.github.com>
2020-01-29 18:59:14 -0500

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,8 +1,8 @@
-//upon clicking a photo, (1) modal display fades in
-//(2) model-content tag updates with currentImage
-//(3) currentImage is highlighted with a blue border
-//(4) href disabled
-//(5) blue border remains until another photo is clicked
+// Upon clicking a photo, (1) modal display fades in
+// (2) model-content tag updates with currentImage
+// (3) currentImage is highlighted with a blue border
+// (4) href disabled
+// (5) blue border remains until another photo is clicked
 
 $('.photos a').on('click', function openModal() {
   var currentImage = $(this).html()
@@ -14,8 +14,8 @@ $('.photos a').on('click', function openModal() {
   return false
 })
 
-//by clicking 'Close' or modal-background, (1) modal display fades out
-//(2) href disabled
+// By clicking 'Close' or modal-background, (1) modal display fades out
+// (2) href disabled
 
 $('.modal-close, .modal-background').on('click', function closeModal() {
   $('.modal').fadeOut(500)
